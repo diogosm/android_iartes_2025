e309f34e3d6c0447424d36f753874bed314c84c5
DevTitans <devtitans@icomp.ufam.edu.br>
2025-04-29 10:27:25 -0400

    added menu deslizante

---
+++ Códigos/login_e_logado_com_menu/app/src/main/java/com/example/login_e_logado_com_menu/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.login_e_logado_com_menu;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText login;
+    public EditText password;
+    public TextView usuarioinvalido;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //inicializo a tela
+        setContentView(R.layout.activity_main);
+
+        Log.i("Debug", "minha msg");
+
+        //inicializo os campos de login e senha
+        login = findViewById(R.id.editLogin);
+        password = findViewById(R.id.editTextTextPassword);
+        usuarioinvalido = findViewById(R.id.usuarioinvalido);
+
+        EdgeToEdge.enable(this);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void fazLogin(View view){
+        String msg = "O login é " +
+                login.getText().toString() +
+                " e a senha é " +
+                password.getText().toString();
+        if(
+                login.getText().toString().equals("admin")
+                        && password.getText().toString().equals("admin")
+        ){
+            Toast notif = Toast.makeText(this,
+                    msg,
+                    Toast.LENGTH_LONG);
+            notif.show();
+            //acesso uma activity nova através da Intent
+            Intent intent = new Intent(MainActivity.this, tela_logada.class);
+            intent.putExtra("usuario", login.getText().toString());
+
+            startActivity(intent);
+
+            // Se quiserem matar essa activity
+            // finish();
+        }else{
+            //ta errado user e senha
+            usuarioinvalido.setVisibility(View.VISIBLE);
+        }
+    }
+
+}
---
+++ Códigos/login_e_logado_com_menu/app/src/main/java/com/example/login_e_logado_com_menu/tela_logada.java
@@ -0,0 +1,80 @@
+package com.example.login_e_logado_com_menu;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class tela_logada extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    private TextView usuarioLogadoTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_logada);
+
+        // Configurando o Toolbar e o Navigation Drawer
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.menu_open,
+                R.string.menu_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        usuarioLogadoTextView = findViewById(R.id.usuarioLogadoTextView);
+
+        String nomeUsuario = getIntent().getStringExtra("usuario");
+        if (nomeUsuario != null && !nomeUsuario.isEmpty()) {
+            usuarioLogadoTextView.setText("Usuário logado: " + nomeUsuario);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Fecha o drawer se estiver aberto quando o botão voltar clicado
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            Toast.makeText(this, "Clicou em Home", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_perfil) {
+            Toast.makeText(this, "Clicou em Perfil", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_configuracoes) {
+            Toast.makeText(this, "Clicou em Configurações", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_compartilhar) {
+            Toast.makeText(this, "Clicou em Compartilhar", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.nav_sobre) {
+            Toast.makeText(this, "Clicou em Sobre", Toast.LENGTH_SHORT).show();
+        }
+
+        //fecha depois do taoast
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
